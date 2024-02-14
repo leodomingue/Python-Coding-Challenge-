7cc2781fbc1d0a86b2a4a61bd2028826b7b9f3bc
leodomingue <leodomingue2016@gmail.com>
2024-02-14 20:55:51 -0300

    Probando

--- Cube3D.py
+++ Cube3D.py
@@ -1,12 +1,14 @@
 import pygame
 import numpy as np
+import math
 from sys import exit
 
 
 #Variables
 WIDTH, HEIGHT = 1200, 800
-FPS = 60
+FPS = 22
 CENTER = (WIDTH//2, HEIGHT//2)
+angle = 0
 
 POINTS = []
 POINTS.append(np.matrix([-1, -1, 1]))
@@ -19,9 +21,29 @@ POINTS.append(np.matrix([1, 1, 1]))
 POINTS.append(np.matrix([-1, 1, 1]))
 
 PROJECTION_MATRIX = np.matrix([
-    [1,0,0],
-    [0,1,0]
+    [1, 0, 0],
+    [0, 1, 0]
 ])
+
+ROTATION_X = np.matrix([
+    [1, 0,  0],
+    [0, math.cos(angle), math.sin(angle) * -1],
+    [0, math.sin(angle), math.cos(angle)]
+    ])
+
+ROTATION_Y = np.matrix([
+    [math.cos(angle), 0,  math.sin(angle)],
+    [0, 1, 0],
+    [math.sin(angle) * -1, 0, math.cos(angle)]
+    ])
+
+ROTATION_Z = np.matrix([
+    [math.cos(angle), math.sin(angle) * -1,  0],
+    [math.sin(angle), math.cos(angle), 0],
+    [0, 0,  1]
+    ])
+
+
 
 #Classes
 
@@ -30,11 +52,56 @@ class Cube:
         self.points = POINTS
         self.color = "RED"
         self.screen = app.screen
+        self.angle = 0
+        self.rotation_x  = np.matrix([
+                                    [math.cos(angle), 0,  math.sin(angle)],
+                                    [0, 1, 0],
+                                    [math.sin(angle) * -1, 0, math.cos(angle)]
+                                    ])
+        self.rotation_z = np.matrix([
+                                    [math.cos(angle), math.sin(angle) * -1,  0],
+                                    [math.sin(angle), math.cos(angle), 0],
+                                    [0, 0,  1]
+                                    ])
+        
+        self.rotation_matrix = np.eye(3)
+        
+    """def apply_rotation(self, axis, angle):
+        if axis == 'x':
+            rotation_matrix = np.matrix([
+                [1, 0, 0],
+                [0, math.cos(angle), -math.sin(angle)],
+                [0, math.sin(angle), math.cos(angle)]
+            ])
+            
+        elif axis == 'y':
+            rotation_matrix = np.matrix([
+                [math.cos(angle), 0, math.sin(angle)],
+                [0, 1, 0],
+                [-math.sin(angle), 0, math.cos(angle)]
+            ])
+            
+        elif axis == 'z':
+            rotation_matrix = np.matrix([
+                [math.cos(angle), -math.sin(angle), 0],
+                [math.sin(angle), math.cos(angle), 0],
+                [0, 0, 1]
+            ])
+        else:
+            rotation_matrix = np.eye(3)  
+
+        self.rotation_matrix = np.dot(rotation_matrix, self.rotation_matrix)
+        return self.rotation_matrix """
         
         
     def draw_points(self):
+        self.angle += 0.01
         for point in self.points:
-            proyected_point = np.dot(PROJECTION_MATRIX, point.reshape(3,1))
+            rotated = np.dot(self.rotation_x, point.reshape(3,1))
+            rotated = np.dot(self.rotation_x, rotated)
+            
+            
+            proyected_point = np.dot(PROJECTION_MATRIX, rotated)
             x_pos = int(proyected_point[0].item()) * 100  + CENTER[0]
             y_pos = int(proyected_point[1].item())  * 100 + CENTER[1]
             
