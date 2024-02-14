bc1fb820804a7f4985c310443a599a9dbe15cfc9
leodomingue <leodomingue2016@gmail.com>
2024-02-14 20:08:37 -0300

    Añadiendo los puntos

--- Cube3D.py
+++ Cube3D.py
@@ -1,13 +1,46 @@
 import pygame
-import numpy
+import numpy as np
 from sys import exit
 
 
 #Variables
 WIDTH, HEIGHT = 1200, 800
 FPS = 60
+CENTER = (WIDTH//2, HEIGHT//2)
+
+POINTS = []
+POINTS.append(np.matrix([-1, -1, 1]))
+POINTS.append(np.matrix([1, -1, 1]))
+POINTS.append(np.matrix([1, 1, 1]))
+POINTS.append(np.matrix([-1, 1, 1]))
+POINTS.append(np.matrix([-1, -1, 1]))
+POINTS.append(np.matrix([1, -1, 1]))
+POINTS.append(np.matrix([1, 1, 1]))
+POINTS.append(np.matrix([-1, 1, 1]))
+
+PROJECTION_MATRIX = np.matrix([
+    [1,0,0],
+    [0,1,0]
+])
 
 #Classes
+
+class Cube:
+    def __init__(self, app):
+        self.points = POINTS
+        self.color = "RED"
+        self.screen = app.screen
+        
+        
+    def draw_points(self):
+        for point in self.points:
+            proyected_point = np.dot(PROJECTION_MATRIX, point.reshape(3,1))
+            x_pos = int(proyected_point[0].item()) * 100  + CENTER[0]
+            y_pos = int(proyected_point[1].item())  * 100 + CENTER[1]
+            
+            pygame.draw.circle(self.screen, self.color, (x_pos, y_pos), 5)
+        
+    
 
 
 #Main Class
@@ -16,10 +49,12 @@ class App:
         pygame.init()
         self.screen =  pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
+        self.cube = Cube(self)
         
     def run(self):
         while True:
             self.screen.fill("BLACK")
+            self.cube.draw_points()
             
             #Update all the screen
             pygame.display.flip()
@@ -31,6 +66,7 @@ class App:
                     exit()
 
             self.clock.tick(FPS)
+           
 
 #Start App
 if __name__ == "__main__":
