207ff2b4896fd3c217a41908e8351e9239f05eb8
leodomingue <leodomingue2016@gmail.com>
2024-03-10 18:47:02 -0300

    a

--- Cube3D.py
+++ Cube3D.py
@@ -23,10 +23,62 @@ class Cube:
             np.array([1, 1, -1]),
             np.array([-1, 1, -1])
         ]
+
+        self.add_intermediate_points()
         self.projected_points = [
              [n, n] for n in range(len(self.points))
         ]
+    
+    def add_intermediate_points(self):
+        new_points = []
+        for p in range(4):
+            # Interpolar linealmente entre los dos puntos
+            interpolated_points = np.linspace(self.points[p], self.points[(p+1) % 4], num=3, endpoint=False)
+            new_points.extend(interpolated_points[1:])
+            interpolated_points = np.linspace(self.points[p+4], self.points[((p+1) % 4) + 4], num=3, endpoint=False)
+            new_points.extend(interpolated_points[1:])
+            interpolated_points = np.linspace(self.points[p], self.points[(p+4)], num=3, endpoint=False)
+            # Agregar los puntos intermedios a la lista
+            new_points.extend(interpolated_points[1:])  # Omitir el primer punto para evitar duplicados
+
+            
         
+        self.points.extend(new_points)
+        
+        self.points.append([0.333, 0.3333, 1])
+        self.points.append([-0.333, -0.3333, 1])
+        self.points.append([0.333, -0.3333, 1])
+        self.points.append([-0.333, 0.3333, 1])
+        self.points.append([0.333, 0.3333, -1])
+        self.points.append([-0.333, -0.3333, -1])
+        self.points.append([0.333, -0.3333, -1])
+        self.points.append([-0.333, 0.3333, -1])
+#
+        self.points.append([0.333,1, 0.3333])
+        self.points.append([-0.333,1, -0.3333])
+        self.points.append([0.333,1, -0.3333])
+        self.points.append([-0.333,1, 0.3333])
+        self.points.append([0.333,-1, 0.3333])
+        self.points.append([-0.333,-1, -0.3333])
+        self.points.append([0.333,-1, -0.3333])
+        self.points.append([-0.333,-1, 0.3333])
+#
+        self.points.append([1,0.333, 0.3333])
+        self.points.append([1,-0.333, -0.3333])
+        self.points.append([1,0.333, -0.3333])
+        self.points.append([1,-0.333, 0.3333])
+        self.points.append([-1,0.333, 0.3333])
+        self.points.append([-1,-0.333, -0.3333])
+        self.points.append([-1,0.333, -0.3333])
+        self.points.append([-1,-0.333, 0.3333])
+
+
+        
+        
+
+            
+
+
 
     def rotate(self, angle):
         rotation_z = np.array([
@@ -45,8 +97,12 @@ class Cube:
 
         self.points = [np.dot(rotation_x, point) for point in self.points]
         
-    def connect_points(self, i, j, points):
-        pygame.draw.line(app.screen, WHITE, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+    def connect_points(self, i, j, points, color):
+        pygame.draw.line(app.screen, color, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+        
+    #def create_point(self, i , j , points):
+       # pygame.draw.circle(app.screen, RED, ( (points[i][0] + points[j][0]),  points[j][1] ), 5)
+
 
 
 class App:
@@ -72,7 +128,7 @@ class App:
             self.screen.fill(BLACK)
 
             
-            self.angle= 0.02
+            self.angle= 0.002
                 
             self.cube.rotate(self.angle)
             
@@ -86,9 +142,11 @@ class App:
                 i += 1
                 
             for p in range(4):
-                self.cube.connect_points(p, (p+1) % 4, self.cube.projected_points)
-                self.cube.connect_points(p+4, ((p+1) % 4) + 4, self.cube.projected_points)
-                self.cube.connect_points(p, (p+4), self.cube.projected_points)
+                self.cube.connect_points(p, (p+1) % 4, self.cube.projected_points,"RED")
+                #self.cube.create_point(p, (p+1) % 4, self.cube.projected_points)
+                self.cube.connect_points(p+4, ((p+1) % 4) + 4, self.cube.projected_points,"YELLOW")
+                self.cube.connect_points(p, (p+4),self.cube.projected_points,"ORANGE")
+                
 
             pygame.display.update()
 
