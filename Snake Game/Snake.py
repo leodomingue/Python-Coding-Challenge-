33a880a0a68003b091ed61ccb7017589d122102b
leodomingue <leodomingue2016@gmail.com>
2024-03-11 01:53:00 -0300

    adding png

--- Snake Game/Snake.py
+++ Snake Game/Snake.py
@@ -2,6 +2,8 @@ import pygame
 import random
 from pygame.math import Vector2
 
+pygame.init()
+
 #Variables
 FPS = 60
 CELLS_NUMBER = 20
@@ -13,13 +15,12 @@ SCREEN_UPDATE = pygame.USEREVENT
 class Fruit:
     def __init__(self,app):
         self.app = app
-        
         self.new_random_position()
         
     #Create figure
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(int(self.pos.x * CELLS_SIZE) ,int(self.pos.y *CELLS_SIZE) ,CELLS_SIZE ,CELLS_SIZE)
-        pygame.draw.rect(app.screen,"Red",fruit_rect)
+        fruit_rect = pygame.Rect(int(self.pos.x * CELLS_SIZE) ,int(self.pos.y *CELLS_SIZE) ,CELLS_SIZE,CELLS_SIZE)
+        app.screen.blit(app.apple, fruit_rect)
         
     def new_random_position(self):
         self.x = random.randint(0,CELLS_NUMBER - 1)
@@ -34,13 +35,84 @@ class Snake:
         self.direction = Vector2(1,0)
         self.new_block = False
         
+        self.tail_up = pygame.image.load("Snake Game/Graphics/Original/tail_up.png").convert_alpha()
+        self.tail_down = pygame.image.load("Snake Game/Graphics/Original/tail_down.png").convert_alpha()
+        self.tail_right = pygame.image.load("Snake Game/Graphics/Original/tail_right.png").convert_alpha()
+        self.tail_left =  pygame.image.load("Snake Game/Graphics/Original/tail_left.png").convert_alpha()
+        
+        self.head_down = pygame.image.load("Snake Game/Graphics/Original/head_down.png").convert_alpha()
+        self.head_up = pygame.image.load("Snake Game/Graphics/Original/head_up.png").convert_alpha()
+        self.head_right = pygame.image.load("Snake Game/Graphics/Original/head_right.png").convert_alpha()
+        self.head_left = pygame.image.load("Snake Game/Graphics/Original/head_left.png").convert_alpha()
+        
+        self.body_topleft = pygame.image.load("Snake Game/Graphics/Original/body_topleft.png").convert_alpha()
+        self.body_topright = pygame.image.load("Snake Game/Graphics/Original/body_topright.png").convert_alpha()
+        self.body_bottomleft = pygame.image.load("Snake Game/Graphics/Original/body_bottomleft.png").convert_alpha()
+        self.body_bottomright = pygame.image.load("Snake Game/Graphics/Original/body_bottomright.png").convert_alpha()
+        
+        self.body_horizontal = pygame.image.load("Snake Game/Graphics/Original/body_horizontal.png").convert_alpha()
+        self.body_vertical = pygame.image.load("Snake Game/Graphics/Original/body_vertical.png").convert_alpha()
+        
+        self.head = self.head_right
+        self.tail = self.tail_right
+        
     #Create Snake
     def draw_snake(self):
-        for cell in self.body:
-            x_pos = cell.x
-            y_pos = cell.y
-            cell_rect = pygame.Rect(int(x_pos * CELLS_SIZE), int(y_pos * CELLS_SIZE), CELLS_SIZE, CELLS_SIZE)
-            pygame.draw.rect(app.screen, "Blue", cell_rect)
+        self.update_head()
+        self.update_tail()
+        
+        for index, cell in enumerate(self.body):
+            #Rect for the positioning
+            x_pos = int(cell.x * CELLS_SIZE)
+            y_pos = int(cell.y * CELLS_SIZE)
+            cell_rect = pygame.Rect(x_pos, y_pos, CELLS_SIZE, CELLS_SIZE)
+            
+            #direction of the head
+            if index == 0:
+                app.screen.blit(self.head, cell_rect)
+            #Direction of the Tail
+            elif index == len(self.body) -1:
+                app.screen.blit(self.tail, cell_rect)    
+            else:
+                previous_cell = self.body[index + 1] - cell
+                next_cell = self.body[index - 1] - cell
+                
+                if previous_cell.x == next_cell.x:
+                    app.screen.blit(self.body_vertical, cell_rect)
+                if previous_cell.y == next_cell.y:
+                    app.screen.blit(self.body_horizontal, cell_rect)
+                else:
+                    if previous_cell.x == -1 and next_cell.y == -1 or previous_cell.y == -1 and next_cell.x == -1:
+                        app.screen.blit(self.body_topleft, cell_rect)
+                    elif previous_cell.x == -1 and next_cell.y == 1 or previous_cell.y == 1 and next_cell.x == -1:
+                        app.screen.blit(self.body_bottomleft, cell_rect)
+                    elif previous_cell.x == 1 and  next_cell.y == -1 or previous_cell.y == -1 and next_cell.x == 1:
+                        app.screen.blit(self.body_topright, cell_rect)
+                    elif previous_cell.x == 1 and next_cell.y == 1 or previous_cell.y == 1 and next_cell.x == 1:
+                        app.screen.blit(self.body_bottomright, cell_rect)
+        
+        
+    def update_head(self):
+        relation = self.body[1] - self.body[0]
+        if relation == Vector2(1,0):
+            self.head = self.head_left
+        elif relation == Vector2(-1, 0):
+            self.head = self.head_right
+        elif relation == Vector2(0, 1):
+            self.head = self.head_up
+        elif relation == Vector2(0, -1):
+            self.head = self.head_down
+            
+    def update_tail(self):
+        relation = self.body[len(self.body) -2] - self.body[len(self.body) -1]
+        if relation == Vector2(1,0):
+            self.tail = self.tail_left
+        elif relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif relation == Vector2(0, -1):
+            self.tail = self.tail_down
             
     #Moving Snake
     def move_snake(self):
@@ -95,6 +167,7 @@ class App:
         self.screen =  pygame.display.set_mode((CELLS_NUMBER * CELLS_SIZE, CELLS_NUMBER * CELLS_SIZE))
         self.clock = pygame.time.Clock()
         self.main_game = Main(self)
+        self.apple = pygame.image.load("Snake Game/apple.png").convert_alpha()
         
         
     def run(self):
@@ -129,7 +202,6 @@ class App:
 
         
 if __name__ == "__main__":
-    pygame.init()
     SCREEN_UPDATE = pygame.USEREVENT
     pygame.time.set_timer(SCREEN_UPDATE, 150)
     app = App()
