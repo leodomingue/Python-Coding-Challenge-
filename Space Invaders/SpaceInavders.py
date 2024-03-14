2dabcb10998f65eb7833396469dce9fc53cdc5e0
leodomingue <leodomingue2016@gmail.com>
2024-03-13 22:21:17 -0300

    ADD OBSTACLES CLASS

--- Space Invaders/SpaceInavders.py
+++ Space Invaders/SpaceInavders.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from player import Player
+import obstacle
 
 #VARIABLE
 WIDTH = 800
@@ -14,13 +15,39 @@ BLACK = (0,0,0)
 class Game:
     def __init__(self, app):
         self.app = app
+        
+        #PLayer setup
         player_sprite = Player((WIDTH/2,HEIGHT), WIDTH, HEIGHT)
         self.player = pygame.sprite.GroupSingle(player_sprite)
+        
+        # obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.block = pygame.sprite.Group()
+        self.obtacle_amount = 4
+        self.obstacle_x_positions = [num * (WIDTH/ self.obtacle_amount) for num in range(self.obtacle_amount)]
+        self.create_multiple_obstacle(WIDTH/20, 650, *self.obstacle_x_positions)
+        
+    def create_obstacle(self, x_start, y_start, offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index, col in enumerate(row):
+                if col == "x":
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size, (240, 79, 80), x, y)
+                    self.block.add(block)
+                    
+    def create_multiple_obstacle(self, x_start, y_start, *offset):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
+        
     
     def run_game(self):
-        self.player.sprite.shoots.draw(app.screen)
         self.player.update()
+        
+        self.player.sprite.shoots.draw(app.screen)
         self.player.draw(app.screen)
+        self.block.draw(app.screen)
 
 class App:
     def __init__(self):
